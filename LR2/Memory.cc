ff0ddfaa737c0896f929592981de9bf32beb63c4
airy13311 <75952796+airy13311@users.noreply.github.com>
2020-12-14 02:58:37 +0500

    Update Memory.cc

--- LR2/Memory.cc
+++ LR2/Memory.cc
@@ -1,46 +1,82 @@
+
 #include <fstream>
 #include "Memory.h"
 
-ROM::ROM(const char * filename)
-{
+ROM::ROM(const char * filename) {
 	std::fstream romfile;
 	romfile.open(filename, std::ios::in | std::ios::ate);
 	_contents.resize(romfile.tellg());
 	romfile.seekg(0);
 	romfile.read(reinterpret_cast<char*>(&_contents[0]), _contents.size());
-	_addr_mask = _contents.size() - 1;
+	_address_mask = _contents.size() - 1;
 }
 
-void AddressSpace::write(unsigned address, uint8_t value, bool io)
-{
+void AddressSpace::write(unsigned address, uint8_t value, bool io) {
 	if (io == true)
 		_io.write(address, value);
-	else
-		_ram.write(address, value);
-}
-
-uint8_t AddressSpace::read(unsigned address, bool io)
-{
-	if (io == true)
-		return _io.read(address);
 	else {
-		if (address < 16384)
-			return _rom.read(address);
-		else
-			return _ram.read(address);
+		unsigned seg = address >> 14;
+		unsigned off = address & 0x3fff;
+
+		switch (seg) {
+			case 0:
+				break;
+
+			case 1:
+				_ram.write((_io.shadow() ? 7 : 5) << 14 | off, value);
+				break;
+
+			case 2:
+				_ram.write(2 << 14 | off, value);
+				break;
+
+			case 3:
+				_ram.write(_io.bank() << 14 | off, value);
+				break;
+
+			default:
+				break;
+		}
 	}
 }
 
-void AddressSpace::write16(unsigned address, uint16_t value)
-{
-	write(address,     (value >> 0) & 0x00ff);
-	write(address + 1, (value >> 8) & 0x00ff);
-}
+uint8_t AddressSpace::read(unsigned address, bool io) {
 
-uint16_t AddressSpace::read16(unsigned address)
-{
+	if (io == true)
+		return _io.read(address);
+	else {
+		unsigned seg = address >> 14;
+		unsigned off = address & 0x3fff;
+
+		switch (seg) {
+			case 0:
+			    return _rom.read(_io.rom() << 14 | off);
+
+			case 1:
+			    return _ram.read((_io.shadow() ? 7 : 5) << 14 | off);
+
+			case 2:
+			    return _ram.read(2 << 14 | off);
+
+			case 3:
+			    return _ram.read(_io.bank() << 14 | off);
+
+			default:
+			    return 0;
+		}
+	}
+}
+void AddressSpace::write16(unsigned address, uint16_t value) {
+
+	write(address, (value >> 0) & 0x00ff);
+	write(address + 1, (value >> 8) & 0x00ff);
+
+}
+uint16_t AddressSpace::read16(unsigned address) {
+
 	uint16_t result = 0;
 	result |= read(address);
 	result |= uint16_t(read(address)) << 8;
 	return result;
+
 }
